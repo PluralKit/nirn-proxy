22d332c5d541ff5799a5def92e56e77de9b19cc1
alyssa <alyssa@mailc.net>
2025-04-06 13:59:28 +0000

    fdaa

--- lib/memberlist.go
+++ lib/memberlist.go
@@ -12,7 +12,7 @@ import (
 func InitMemberList(advertiseAddr string, knownMembers []string, port int, proxyPort string, manager *QueueManager) *memberlist.Memberlist {
 	config := memberlist.DefaultLANConfig()
 	config.BindPort = port
-	if os.Getenv("FORCE_BIND_FDEF") != "true" && advertiseAddr != "" {
+	if os.Getenv("FORCE_BIND_FDAA") != "true" && advertiseAddr != "" {
 		// i'm pretty sure being able to pass a dns name into BIND_IP is a quirk of go's http server
 		// but it's useful for us, so let's handle that for memberlist as well
 		if net.ParseIP(advertiseAddr) == nil {
@@ -29,7 +29,7 @@ func InitMemberList(advertiseAddr string, knownMembers []string, port int, proxy
 	}
 	// i'm tired of fighting nomad/docker
 	// just get this working
-	log.Printf("hi %v\n", os.Getenv("FORCE_BIND_FDEF"))
+	log.Printf("hi %v\n", os.Getenv("FORCE_BIND_FDAA"))
 	if os.Getenv("FORCE_BIND_FDEF") == "true" {
 		config.AdvertiseAddr = func() string {
 			iface, err := net.InterfaceByName("eth0")
@@ -42,7 +42,7 @@ func InitMemberList(advertiseAddr string, knownMembers []string, port int, proxy
 			}
 			for _, v := range addrs {
 				log.Printf("find fdef:: got addr %v\n", v.String())
-				if strings.Contains(v.String(), "fdef:") {
+				if strings.Contains(v.String(), "fdaa:") {
 					return strings.Split(v.String(), "/")[0]
 				}
 			}
